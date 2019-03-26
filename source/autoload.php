0f656d12a1ec008034421c2ca7a8b3492025d074
Julien Delsescaux <jdelsescaux@skillbase.fr>
2019-03-26 16:44:14 +0100

    synchro

---
+++ source/autoload.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+spl_autoload_register(function ($calledClassName) {
+
+    $normalizedClassName = preg_replace('`^\\\\`', '', $calledClassName);
+
+
+    if(strpos($normalizedClassName, 'Phi\Traits') === 0) {
+
+        $relativeClassName = str_replace('Phi\Traits', '', $normalizedClassName);
+        $relativePath = str_replace('\\', '/', $relativeClassName);
+
+
+        $definitionClass = __DIR__.'/class'.$relativePath.'.php';
+        if(is_file($definitionClass)) {
+            include($definitionClass);
+        }
+    }
+
+
+
+});
+
+
